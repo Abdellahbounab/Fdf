1ba41e1850a87466d7ec4980bd2faa8ce50ed631
Abdellah Bounab <abounab@c1r4p2.1337.ma>
2024-03-07 22:03:12 +0100

    update

--- Makefile
+++ Makefile
@@ -10,15 +10,11 @@ CFLAGS = -Wall -Wextra -Werror
 
 all : ${NAME}
 
-${NAME} : ${SRCS_OBJ}
-		${CC} ${CFLAGS} ${SRCS_OBJ} -o $@
-
 $(NAME): $(SRCS_OBJ)
-	$(CC) $(SRCS_OBJ) -Lmlx_linux -lmlx_Linux -L/usr/lib -Imlx_linux -lXext -lX11 -lm -lz -o $(NAME)
-
+	$(CC) $(SRCS_OBJ) -lmlx -framework OpenGL -framework AppKit -o $(NAME)
 
 %.o: %.c
-	$(CC) ${CFLAGS} -I/usr/include -Imlx_linux -O3 -c $< -o $@
+	$(CC) -Wall -Wextra -Werror -Imlx -c $< -o $@
 
 clean :
 		rm -rf *.o
--- main.c
+++ main.c
@@ -1,7 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abounab <abounab@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/07 21:19:24 by abounab           #+#    #+#             */
+/*   Updated: 2024/03/07 21:28:49 by abounab          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdio.h>
 #include <mlx.h>
 
-int main()
+int	main(void)
 {
-    printf("Hello, World !");
-}       
+	void	*mlx;
+	void	*mlx_win;
+
+	mlx = mlx_init();
+	mlx_win = mlx_new_window(mlx, 1500, 500, "Abounab");
+	mlx_loop(mlx);
+}
